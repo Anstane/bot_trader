6d5dd958ef8edb9295ef55cdcb63df085d7dd44b
Mikhail Moskovkin <anstane228@gmail.com>
2024-02-07 02:08:17 +0300

    DB

--- bot_trader/bot.py
+++ bot_trader/bot.py
@@ -1,16 +1,16 @@
 import os
 import asyncio
-import uuid
 
+from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart, Command
-from dotenv import load_dotenv
+
+from db import create_database, add_user_wallet, get_user_wallets, delete_user_wallet
 
 load_dotenv()
+
 TOKEN = os.getenv('TELEGRAM_TOKEN')
 dp = Dispatcher()
-
-user_wallets = {}
 
 
 @dp.message(CommandStart())
@@ -24,6 +24,7 @@ async def start(message: types.Message):
 @dp.message(Command("add_wallet"))
 async def add_wallet(message: types.Message):
     """Функция которая отвечает за добавление кошельков пользователя."""
+
     wallet_info = message.text.split()[1:] # Разбираем сообщение.
 
     # Проверяем, что сообщение содержит кошелёк. (Надо будет добавить проверку форматов)
@@ -39,37 +40,28 @@ async def add_wallet(message: types.Message):
         return
 
     user_id = message.from_user.id # Получаем ID юзера.
+    wallet_address = wallet_info[0] # Получаем адрес кошелька.
 
-    # Если юзер не был ранее зарегистрирован.
-    if user_id not in user_wallets:
-        user_wallets[user_id] = {}
-
-    # Проверяет все адреса кошельков и если такой кошелёк уже был добавлен any() выдаёт False.
-    if any(wallet_info[0] == wallet_data for wallet_data in user_wallets[user_id].values()):
-        await message.answer(f"Кошелек с адресом '{wallet_info[0]}' уже был добавлен.")
-        return
-
-    # Генерируем уникальный идентификатор кошелька
-    wallet_id = str(uuid.uuid4())[:8]
-
-    user_wallets[user_id][wallet_id] = wallet_info[0] # Добавляем все данные в словарь.
-
-    await message.answer(f"Крипто кошелёк с адресом '{wallet_info[0]}' был успешно добавлен. ID кошелька: {wallet_id}")
+    if await add_user_wallet(user_id, wallet_address): # Создаём или получаем объект.
+        await message.answer(f"Криптокошелёк с адресом '{wallet_info[0]}' был успешно добавлен.")
+    else:
+        await message.answer(f"Криптокошелёк с адресом '{wallet_info[0]}' уже существует.")
 
 
 @dp.message(Command("wallets"))
 async def wallets(message: types.Message):
     """Функция для отображение кошельков пользователя."""
+
     user_id = message.from_user.id # Получаем ID пользователя.
-    wallets = user_wallets.get(user_id) # Получаем кошельки пользователя.
+    wallets = await get_user_wallets(user_id) # Получаем кошельки пользователя.
 
     if not wallets:
         await message.answer("У вас пока нет ни одного криптокошелька.")
         return
 
     text = "Ваши кошельки: \n"
-    for id, address in wallets.items(): # Перебираем кошельки пользователя.
-        text += f"{id} - {address} \n"
+    for wallet in wallets: # Перебираем кошельки пользователя.
+        text += f"{wallet.wallet_address} \n"
 
     await message.answer(text)
 
@@ -77,38 +69,29 @@ async def wallets(message: types.Message):
 @dp.message(Command("delete_wallet"))
 async def delete_wallet(message: types.Message):
     """Функция для удаления кошелька."""
+
     user_id = message.from_user.id # Получаем ID пользователя.
-    wallets = user_wallets.get(user_id) # Получаем кошельки пользователя.
-
-    if not wallets:
-        await message.answer("У вас пока нет ни одного криптокошелька.")
-        return
-
-
-    args = message.text.split()[1:]
+    args = message.text.split()[1:] # Разбираем сообщение.
 
     if not args:
-        await message.answer("Вы не указали ID для удаления кошелька.")
+        await message.answer("Вы не указали адрес кошелька для удаления.")
         return
 
-    wallet_id = args[0]
-    
-    if wallet_id not in wallets:
-        await message.answer(f"Кошелек с ID {wallet_id} не найден.")
-        return
-    
-    wallet_on_delete = user_wallets[user_id][wallet_id] # Получаем кошелёк.
+    wallet_address = args[0] # Получаем адрес кошелька.
 
-    # Удаляем кошелек с указанным ID
-    del user_wallets[user_id][wallet_id]
+    deleted = await delete_user_wallet(user_id, wallet_address) # Пытаемся удалить кошелёк.
 
-    # Отправляем сообщение об успешном удалении кошелька
-    await message.answer(f"Криптокошелёк ({wallet_on_delete}) был успешно удалён.") 
+    if deleted:
+        await message.answer(f"Криптокошелёк с адресом '{wallet_address}' был успешно удалён.")
+    else:
+        await message.answer(f"Криптокошелёк с адресом '{wallet_address}' не найден.")
 
 
 async def main() -> None:
     """Функция запускает бота."""
+
     bot = Bot(TOKEN)
+    await create_database()
     await dp.start_polling(bot, skip_updates=True)
 
 
--- bot_trader/db.py
+++ bot_trader/db.py
@@ -1,25 +1,92 @@
 import os
+import asyncio
 
-from sqlalchemy import create_engine, text
-from sqlalchemy.engine.url import URL
-from sqlalchemy.orm import declarative_base
 from dotenv import load_dotenv
+
+from sqlalchemy import Column, String, Integer, select, delete
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 
 load_dotenv()
 
-DATABASE = {
-    'drivername': 'postgresql+psycopg2',
-    'host': os.getenv("DB_HOST"),
-    'port': os.getenv("DB_PORT"),
-    'username': os.getenv("DB_USER"),
-    'password': os.getenv("DB_PASSWORD"),
-    'database': os.getenv("DB_NAME")
-}
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
 
-engine  = create_engine(URL.create(**DATABASE))
+engine  = create_async_engine(f"postgresql+asyncpg://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}", echo=True)
+async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
-# Base = declarative_base()
+Base = declarative_base()
 
-# with engine.connect() as conn:
-#     res = conn.execute(text("SELECT VERSION()"))
-#     print(f'{res=}')
+class Wallet(Base):
+    __tablename__ = "wallets"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer)
+    wallet_address = Column(String)
+
+async def create_database() -> None:
+    """Функция для создания БД."""
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all) # Стираем все старые таблицы.
+        await conn.run_sync(Base.metadata.create_all) # Создаём новые таблицы.
+
+async def add_user_wallet(user_id: int, wallet_address: str) -> bool:
+    """Функция для добавления кошелька в БД."""
+
+    async with async_session() as session:
+        # Проверяем есть ли в БД запись с полученными данными.
+        select_wallet = select(Wallet).where(
+            (Wallet.user_id == user_id) & (Wallet.wallet_address == wallet_address)
+        )
+        result = await session.execute(select_wallet)
+        existing_wallet = result.scalar() # Этот метод возвращает объект или None.
+
+        if existing_wallet: # Если объект существует - возвращаем False.
+            return False
+
+        # В обратном случае - создаём и возвращаем True.
+        new_wallet = Wallet(user_id=user_id, wallet_address=wallet_address)
+        session.add(new_wallet)
+        await session.commit()
+        return True
+
+async def get_user_wallets(user_id: int) -> list:
+    """Функция, которая получает список существующих кошельков."""
+
+    async with async_session() as session:
+        select_wallet = select(Wallet).where((Wallet.user_id == user_id))
+        result = await session.execute(select_wallet)
+        return result.scalars().all()
+
+async def delete_user_wallet(user_id: int, wallet_address: str) -> bool:
+    """Функция для удаления выбранного кошелька."""
+
+    async with async_session() as session:
+        # Проверяем есть ли в БД запись с полученными данными.
+        select_wallet = select(Wallet).where(
+            (Wallet.user_id == user_id) & (Wallet.wallet_address == wallet_address)
+        )
+        result = await session.execute(select_wallet)
+        existing_wallet = result.scalar() # Этот метод возвращает объект или None.
+
+        if existing_wallet: # Если объект существует - удаляем и возвращаем True.
+            select_wallet = delete(Wallet).where(
+                (Wallet.user_id == user_id) & (Wallet.wallet_address == wallet_address)
+            )
+            await session.execute(select_wallet)
+            await session.commit()
+            return True
+
+        return False # Если объект не найден, возвращаем False.
+
+async def main() -> None:
+    """Вспомогательная функция для запуска БД."""
+
+    await create_database()
+
+if __name__ == "__main__":
+    asyncio.run(main())
